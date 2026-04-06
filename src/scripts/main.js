4caaf3294d46b4e927b51e3e222082b980dc57ca
Natalia Lunkevych <gadyka08@gmail.com>
2026-04-06 17:08:01 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,31 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  // чи об'єкт не порожній.перевірка.
+  // Якщо ключів немає не потрібно створювати список.
+  if (Object.keys(data).length === 0) {
+    return;
+  }
+
+  // робимо основний елемент списку <ul>
+  const ul = document.createElement('ul');
+
+  // ітеруємося
+  for (const key in data) {
+    // робимоелемент пункту списку <li>
+    const li = document.createElement('li');
+    li.textContent = key; // пишемоназву ключа в текст елемента
+
+    //  РЕКУРСІЯ: викликаємо цю ж функцію для внутрішнього об'єкта.
+    // Передаємо поточний <li> як новий контейнер і вкладений об'єкт data[key].
+    createTree(li, data[key]);
+
+    // Додаємо готовий <li> до нашого <ul>
+    ul.append(li);
+  }
+
+  // додаємо весь побудований список у початковий контейнер
+  container.append(ul);
 }
 
 createTree(tree, food);
